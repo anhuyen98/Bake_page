807ddb693cb036e994ea74e3dc6c2adf969f3c69
anhuyen98 <eros3898@gmail.com>
2024-08-02 20:44:19 +0700

    re-practice skills

---
+++ js/index.js
@@ -0,0 +1,8 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options for a specific gallery
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  items: 1,
+});
+$(".counter").countUp();
